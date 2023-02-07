862952426c2fa6ed686ff6f61030a168cc44c663
alenanovikovashahove0307 <novikova-fin@ukr.net>
2023-02-07 21:32:38 +0200

    Додано з файли css

--- js/menu.js
+++ js/menu.js
@@ -5,10 +5,10 @@
     menu: document.querySelector("[data-menu]"),
   };
 
-  refs.openMenuBtn.addEventListener("click", toggleMenu);
-  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  refs.openMenuBtn.addEventListener("click", toggleModal);
+  refs.closeMenuBtn.addEventListener("click", toggleModal);
 
-  function toggleMenu() {
-    refs.menu.classList.toggle("is-hidden");
+  function toggleModal() {
+    refs.menu.classList.toggle("is-open");
   }
 })();
